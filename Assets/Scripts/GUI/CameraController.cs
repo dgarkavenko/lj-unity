82dcf42a07ea54ab0625d986e57bb21705c66227
antonsmetanin <Anton.Smetanin@gmail.com>
2014-11-13 15:08:19 +0300

    tree material, jump height, camera scroll

--- Assets/Scripts/GUI/CameraController.cs
+++ Assets/Scripts/GUI/CameraController.cs
@@ -8,6 +8,7 @@ public class CameraController : MonoBehaviour {
     Camera cam;
     public float scale = 1f;
     public bool update = false;
+	public Lumberjack lumberjack;
 
 
     string s;
@@ -33,6 +34,19 @@ public class CameraController : MonoBehaviour {
 	
 	
 	void Update () {
-        if (update) cam.orthographicSize = Screen.height * 0.5f / scale / PIXELS_IN_METER;	
+        if (update) cam.orthographicSize = Screen.height * 0.5f / scale / PIXELS_IN_METER;
+	}
+
+
+	void FixedUpdate()
+	{
+		var target = lumberjack.transform.position;
+		var camPos = transform.position;
+		
+		if (target.x != camPos.x) {
+			camPos.x = Mathf.Lerp(camPos.x, target.x, 0.05f);
+		}
+		
+		transform.position = camPos;
 	}
 }
