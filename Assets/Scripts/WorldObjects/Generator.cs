2d281643d275d7542d2b6cd5d7bdf4c051db34c8
Anton Smetanin <Anton.Smetanin@gmail.com>
2014-11-25 17:22:04 +0300

    parallax and simple generators

---
+++ Assets/Scripts/WorldObjects/Generator.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Generator : MonoBehaviour
+{
+	public SpriteRenderer source;
+	public int startX;
+	public int endX;
+
+
+	void Start()
+	{
+		var spriteWidth = source.sprite.bounds.size.x;
+		source.transform.localPosition = new Vector3(startX, source.transform.localPosition.y, source.transform.localPosition.z);
+		var currentX = startX + spriteWidth;
+
+		while (currentX < endX) {
+			var newPosition = source.transform.localPosition;
+			newPosition.x = currentX;
+
+			var newObject = ((SpriteRenderer)GameObject.Instantiate(source)).transform;
+			newObject.parent = source.transform.parent;
+			newObject.localPosition = newPosition;
+			newObject.localScale = Vector3.one;
+			newObject.name = source.name;
+
+			currentX += spriteWidth;
+		}
+	}
+}
---
+++ Assets/Scripts/WorldObjects/ParallaxManager.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+
+public class ParallaxManager : MonoBehaviour
+{
+	[Serializable]
+	public class Layer
+	{
+		public Transform container;
+		public float distance;
+		public Vector2 pivotPoint;
+	};
+
+	public CameraController cameraController;
+	public Layer[] layers;
+	
+
+	void Update()
+	{
+		var cameraCenter = new Vector2(cameraController.transform.position.x, cameraController.transform.position.y);
+
+		foreach (var layer in layers) {
+			var layerShift = cameraCenter / (layer.distance + 1f);
+			layer.container.position = new Vector3(layerShift.x, layer.container.position.y, layer.container.position.z);
+		}
+	}
+}
